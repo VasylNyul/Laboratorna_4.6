6008d4d00ae22a3a1fb657893e1ec1a5e1acaa1c
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2021-10-21 13:32:29 +0300

    wowowowow

---
+++ Laba_4__6.hpp
@@ -0,0 +1,13 @@
+//
+//  Laba_4__6.hpp
+//  Laboretorna_4_7
+//
+//  Created by apple on 21.10.2021.
+//
+
+#ifndef Laba_4__6_hpp
+#define Laba_4__6_hpp
+
+#include <stdio.h>
+
+#endif /* Laba_4__6_hpp */
